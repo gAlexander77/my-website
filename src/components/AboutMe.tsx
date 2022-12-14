27718bee80712697cfa7d76709da38d52a1f435c
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2022-12-13 23:27:09 -0600

    Update

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutMe(){
+    return (
+        <div>
+            <p>
+                My name is Alexander I am a computer science student at the University of North Texas
+            </p>
+
+        </div>
+    );
+}
+
+export default AboutMe;
