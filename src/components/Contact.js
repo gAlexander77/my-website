f476203a333e99c181bb8e4a3625eca689a61a40
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-01-09 18:17:34 -0600

    Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { MdEmail } from "react-icons/md";
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import '../style/components/Contact.css';
 
 function Contact(props){
 
     return(
         <div className="contact-container">
-            Contact Me
+            <h1 className="contact-title">My Contacts</h1>
+            <div className="contact-list-of-contacts">        
+                <button className="btn"><BsGithub/>Github</button>
+                <button className="btn"><BsLinkedin/>LinkedIn</button>
+                <p><MdEmail/>galexander1099@gmail.com</p>
+            </div>
         </div>
     );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { BsGithub, BsYoutube } from 'react-icons/bs';
 import '../style/components/Projects.css';
 
 function Projects(props){
 
+
     return(
         <div className="projects-container">
-            Projects
+            <h1 className="projects-title" >Projects</h1>
+            <div className="projects-list-of-projects"> 
+                <div className="project-container">
+                    <h1>Recipe Application</h1>
+                    <p>Javascript | Python | React | Flask | SQLite3</p>
+                    <div className="project-links">
+                        <a href='https://github.com/gAlexander77/recipe-application/'>
+                            <button className="btn"><BsGithub/>Github</button>
+                        </a>
+                    </div>
+                </div>
+                <div className="project-container">
+                    <h1>My Planner</h1>
+                    <p>Javascript | React | Express | Node | MongoDB</p>
+                    <div className="project-links">
+                        <button className="btn"><BsGithub/>Github</button>
+                    </div>
+                </div>
+                <div className="project-container">
+                    <h1>Study Timer</h1>
+                    <p>React | Javascript | HTML | CSS</p>
+                    <div className="project-links">
+                        <button className="btn"><BsGithub/>Github</button>
+                    </div>
+                </div>
+                <div className="project-container">
+                    <h1>Auto Loan Calculator</h1>
+                    <p>React | Javascript | HTML | CSS</p>
+                    <div className="project-links">
+                        <button className="btn"><BsGithub/>Github</button>
+                    </div>
+                </div>
+            </div>   
         </div>
     );
 }
